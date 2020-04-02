29ea2d68e49b820c2d390a312d32cf20ca5e2000
Mauricio de Oliveira Dian <mauricio.dian@outlook.com>
2020-04-02 15:57:57 -0300

    AlteracaoMenuMediaBotaoWhats

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -17,3 +17,16 @@ jQuery("document").ready(function($) {
 	});
 
 });
+
+// Botão Whats
+// var displayWhats = document.getElementById("whatswidget-conversation").style.display = "none";
+// var opacityWhats = document.getElementById("whatswidget-conversation").style.opacity = "0";
+
+// var button = document.getElementById("whatswidget-button");
+// button.addEventListener("click", openChat);
+
+// var conversationMessageOuter = document.getElementById("whatswidget-conversation-message-outer");
+// conversationMessageOuter.addEventListener("click", openChat);
+
+// var chatOpen = !1;
+// function openChat() { 0 == chatOpen ? (displayWhats = "block", opacityWhats = 100, chatOpen = !0, conversationMessageOuter.style.display = "none") : (opacityWhats = 0, displayWhats = "none", chatOpen = !1) }
